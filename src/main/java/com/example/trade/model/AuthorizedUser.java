8885255fb98955b6e2d9f04a79a1f8e7449366ad
nazario17 <hazarkorchak@gmail.com>
2024-09-26 20:23:38 +0300

    Update AuthorizedUser.java

--- src/main/java/com/example/trade/model/AuthorizedUser.java
+++ src/main/java/com/example/trade/model/AuthorizedUser.java
@@ -5,10 +5,13 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import lombok.Data;
+
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
 @Entity
 public class AuthorizedUser extends User {
 
